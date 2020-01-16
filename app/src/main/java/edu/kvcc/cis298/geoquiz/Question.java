e6b685681b3ea51309bb1c567563a1afdb1adc55
David Barnes <dbarnes@kvcc.edu>
2020-01-16 17:14:23 -0500

    Update app to use a Question class and multiple questions.

---
+++ app/src/main/java/edu/kvcc/cis298/geoquiz/Question.java
@@ -0,0 +1,29 @@
+package edu.kvcc.cis298.geoquiz;
+
+public class Question {
+    // This will be a pointer to one of the string constants
+    private int mTextResId;
+    // Whether the question is true or not.
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
--- app/src/main/java/edu/kvcc/cis298/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/kvcc/cis298/geoquiz/QuizActivity.java
@@ -4,27 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    // Array of questions to display
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+    // Counter to keep track of which question we are on.
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        // Get a handle to the TextView for the question text
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(
-                        QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT
-                ).show();
+                checkAnswer(true);
             }
         });
 
@@ -32,12 +47,56 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(
-                        QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT
-                ).show();
+                checkAnswer(false);
+            }
+        });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Increment the index and mod by the array length
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
     }
+
+    private void updateQuestion() {
+        // Get the string resource id for the current question.
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        // Set the question text
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        // Get whether the current question is true or not
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        // Setup a int for what message to toast. Default to 0. Will be changed below.
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            // If user pressed true and question is true
+            messageResId = R.string.correct_toast;
+        } else {
+            // Else must be false
+            messageResId = R.string.incorrect_toast;
+        }
+
+        // Toast out the correct message
+        Toast.makeText(
+                this,
+                messageResId,
+                Toast.LENGTH_SHORT
+        ).show();
+    }
 }
+
+
+
+
+
+
+
+
+

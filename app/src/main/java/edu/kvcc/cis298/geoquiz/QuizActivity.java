efba3d8eb641e20bc8a11b8b53346465c6955f03
David Barnes <dbarnes@kvcc.edu>
2020-01-21 17:40:34 -0500

    Fix bug where question index would not be saved on screen rotation.

--- app/src/main/java/edu/kvcc/cis298/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/kvcc/cis298/geoquiz/QuizActivity.java
@@ -2,12 +2,20 @@ package edu.kvcc.cis298.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    // Setup a string constant to use as a TAG for filtering in LogCat
+    private static final String TAG = "QuizActivity";
+    // String constant to use as a key for storing data into the
+    // bundle object. Key will be used in onSaveInstanceState and
+    // in onCreate whe pulling out.
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -29,7 +37,19 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Log out a message to the LogCat
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        // Check the Bundle and see if there is data in it.
+        // If so, fetch it out and use it to set the current
+        // question index.
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(
+                    KEY_INDEX,
+                    0
+            );
+        }
 
         // Get a handle to the TextView for the question text
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -60,6 +80,49 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        // This method gets called around the same time as
+        // onPause. It is done before onStop is called.
+        // We can save out any information we want to get
+        // back out on screen rotation.
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        // Store the current question index in the Bundle with
+        // a key of KEY_INDEX.
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
     }
 
     private void updateQuestion() {
